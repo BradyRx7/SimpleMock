017c37cf06a6d60b7388d3a3331c6cd0b092a2af
Brady Reed <bradyrx7@gmail.com>
2018-06-11 23:58:13 -0400

    Maven skeleton structure added and pom updated

---
+++ src/main/java/edu/bradyreed/advancedjava/StockTicker.java
@@ -0,0 +1,13 @@
+package edu.bradyreed.advancedjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class StockTicker 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
